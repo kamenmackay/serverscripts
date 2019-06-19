fcdb9432866fd4d510d5618c4b47221b1416afc3
Kamen MacKay <kamen.mackay@gmail.com>
2019-06-19 09:39:54 +0100

    Works but needs better error handling and function for directory creation

---
+++ influxbackups/influxbackup.py
@@ -0,0 +1,32 @@
+
+#~/usr/bin/python
+
+# Make backup dir in influxbackup with date as directory name
+# Change into current date directory
+# Backup data from remote influxdb into current date dir
+
+
+import os, sys
+import datetime
+import subprocess
+from influxdb import InfluxDBClient
+
+influxbackuppath = "/data/influxbackups"
+now = datetime.datetime.now()
+currentdate = now.strftime('%m%d%Y')
+bupath = influxbackuppath + "/" + currentdate
+# Make the below into a function
+if (os.path.exists(bupath)):
+  print(bupath + " already exists so proceeding...")
+else:
+  os.mkdir( bupath, 755 );
+  print("Path is created")
+
+client = InfluxDBClient(host='10.1.2.16', port=8086)
+dbs = client.get_list_database()
+# print(dbs)
+test = [k.values()[0] for k in dbs]
+print(str(test))
+for x in test:
+ os.mkdir( bupath + "/" + x, 755)
+ subprocess.call(["/home/kmackay/influxd","backup","-portable","-database", x, "-host","10.1.2.16:8088", bupath + "/" + x])
